6ff0a4e0b16d8dfba34f4c303f6fbb49b34dcd53
Yoon321 <tkdwls734@gmail.com>
2024-02-27 18:42:45 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/example/jenkinsprac/JenkinsPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsprac/JenkinsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
